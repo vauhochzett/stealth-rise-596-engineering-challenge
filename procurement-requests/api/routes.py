f370c05db0b62191bf949e62777c60221a96c840
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2025-12-09 22:54:38 +0100

    Add endpoints for request handling

--- procurement-requests/api/routes.py
+++ procurement-requests/api/routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.templating import Jinja2Templates
+
+import api.data
+from api.models import ProcurementRequest
 
 templates = Jinja2Templates(directory="html")
 router = APIRouter()
@@ -8,3 +11,17 @@ router = APIRouter()
 @router.get("/")
 async def index(request: Request):
     return templates.TemplateResponse(request=request, name="index.html", context={})
+
+
+@router.get("/requests")
+async def get_requests():
+    return api.data.get_requests()
+
+
+@router.post("/request/new")
+async def add_request(request: ProcurementRequest):
+    success: bool = api.data.add_request(request)
+    if not success:
+        raise HTTPException(status_code=500, detail="request could not be created")
+
+    return {"message": "Request created"}
